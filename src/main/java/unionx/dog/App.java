4fea314be57c69509c47f1252dc7561ef139c7aa
unionx <xudi1990@gmail.com>
2012-10-22 18:54:45 +0800

    maven init a new project

---
+++ src/main/java/unionx/dog/App.java
@@ -0,0 +1,13 @@
+package unionx.dog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
